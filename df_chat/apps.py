3ee18a7c647bd1c09fc06ec417be7a50087fcb63
eugapx <111432881+eugapx@users.noreply.github.com>
2023-05-15 17:02:12 +0300

    change URLs

--- df_chat/apps.py
+++ df_chat/apps.py
@@ -4,8 +4,8 @@ from django.apps import AppConfig
 class ChatConfig(AppConfig):
     default_auto_field = "hashid_field.BigHashidAutoField"
     name = "df_chat"
-    api_path = "df-chat/"
-    asgi_prefix = "df-chat/"
+    api_path = "chat/"
+    asgi_prefix = "chat/"
 
     def ready(self) -> None:
         # Trigger registering signals for model observers
