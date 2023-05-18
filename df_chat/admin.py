1ce74befbdc8d39175284f9600e6f270f29b4b69
sreekanthreddybalne <sreekanthreddybalne@gmail.com>
2023-05-19 04:39:24 +0530

    Added UserChatAdmin
    Now, the "is_online" column can be found in the UserChat listing page instead of RoomUser

--- df_chat/admin.py
+++ df_chat/admin.py
@@ -2,47 +2,24 @@ from .models import Message
 from .models import MessageImage
 from .models import Room
 from .models import RoomUser
+from .models import UserChat
 from django.contrib import admin
+
+
+@admin.register(UserChat)
+class UserChatAdmin(admin.ModelAdmin):
+    list_display = ("user", "is_online")
+    list_filter = ("is_online",)
 
 
 class RoomUserInline(admin.TabularInline):
     model = RoomUser
 
 
-class IsOnlineFilter(admin.SimpleListFilter):
-    """
-    Human-readable title which will be displayed in the
-    right admin sidebar just above the filter options in the RoomUser change page.
-    """
-
-    title = "is online"
-
-    # Parameter for the filter that will be used in the URL query.
-    parameter_name = "is_online"
-
-    def lookups(self, request, model_admin):
-        return (
-            (True, "Yes"),
-            (False, "No"),
-        )
-
-    def queryset(self, request, queryset):
-        if self.value() == "True":
-            return queryset.filter(user__user_chat__is_online=True)
-        if self.value() == "False":
-            return queryset.filter(user__user_chat__is_online=False)
-
-
 @admin.register(RoomUser)
 class RoomUserAdmin(admin.ModelAdmin):
-    list_display = ("room", "user", "is_active", "get_is_online")
-    list_filter = ("is_active", "room__title", IsOnlineFilter)
-
-    # Using this, the 'is_online' column on the RoomUser change page in the admin dashboard
-    # will display beautiful icons ✅ for online users and ❌ for offline users.
-    @admin.display(description="is_online", boolean=True)
-    def get_is_online(self, obj: RoomUser) -> bool:
-        return obj.is_online
+    list_display = ("room", "user", "is_active")
+    list_filter = ("is_active", "room__title")
 
 
 @admin.register(Room)
